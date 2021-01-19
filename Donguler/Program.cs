a02e2362beb884ce3f15cd2dfb605646fa0c8fb1
Zehra Nur Koyuncu <zehra.koyuncu@metu.edu.tr>
2021-01-19 10:50:52 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Kampı", "Programlamaya Başlangıç İçin Temel Kurs",
+                "Java", "Python", "C#", "C++"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself!!
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmısMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+
+
+
+            if (sistemeGirisYapmısMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
